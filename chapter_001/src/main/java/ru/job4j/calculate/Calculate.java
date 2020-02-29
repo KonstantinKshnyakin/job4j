b2bb1ec98dc93e6ee5f7c2382859a8f599e36eee
Konstantin Kshnyakin <kocmoc007@gmail.com>
2020-03-01 01:35:16 +0300

    5.8. JavaDoc. Code convention[#242161]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+
+/**
+ * Class Calculate
+ * @author K.Kshnyakin
+ * @since 01.03.2020
+ * @version 1
+*/
+public class Calculate {
+
+    /**
+     * Main
+     * @param args - args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Konstantin Kshnyakin
+ * @version 1.0
+ * @since 01.03.2020
+ */
+package ru.job4j.calculate;
