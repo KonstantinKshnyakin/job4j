434aaa4a597247919b84591403e0c8e6feb01cbf
Konstantin Kshnyakin <kocmoc007@gmail.com>
2020-03-02 22:08:47 +0300

    Слово начинается с ...[#242083]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            boolean isEquals = pref[i] == word[i];
+            if (!isEquals) {
+                System.out.println(i );
+                return false;
+            }
+        }
+        return result;
+    }
+}
