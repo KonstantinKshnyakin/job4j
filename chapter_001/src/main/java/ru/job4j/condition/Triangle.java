b613fdb677a644ac40f96164bd1d5da432ff7a78
Konstantin Kshnyakin <kocmoc007@gmail.com>
2020-03-09 13:47:24 +0300

    2.2. Рефакторинг - Площадь треугольника.[#242247]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,6 +2,33 @@ package ru.job4j.condition;
 
 public class Triangle {
 
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ab, Point bc, Point cd) {
+        this.first = ab;
+        this.second = bc;
+        this.third = cd;
+    }
+
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+
     public static boolean exist(double ab, double ac, double bc) {
         return ((ab + ac > bc) && (ac + bc > ab) && (ab + bc > ac));
     }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
@@ -16,5 +18,19 @@ public class TriangleTest {
     public void whenDontExist() {
         boolean result = Triangle.exist(2.0, 2.0, 4.1);
         assertThat(result, is(false));
+    }
+
+    @Test
+    public void area1() {
+        Triangle triangle = new Triangle(new Point(1, 2), new Point(6, 4), new Point(5, 9));
+        double area = triangle.area();
+        assertEquals(area, 13.5, 0.1);
+    }
+
+    @Test
+    public void area2() {
+        Triangle triangle = new Triangle(new Point(0, 0), new Point(8, 2), new Point(3, 6));
+        double area = triangle.area();
+        assertEquals(area, 21, 0.1);
     }
 }
