82277d9caa4a18deb1ca2ca44ad807b89439dbb6
Konstantin Kshnyakin <kocmoc007@gmail.com>
2020-03-12 23:28:38 +0300

    5. Реализовать класс Tracker[#242274]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,84 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(generateId());
+        items[position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    /**
+     * Метод возвращает массив Item`ов без null ячеек.
+     * @return массив Item.
+     */
+    public Item[] findAll() {
+        Item[] itemWithoutNull = new Item[position];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            Item item = items[index];
+            if (item != null) {
+                itemWithoutNull[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemWithoutNull, size);
+    }
+
+    /**
+     * Метод возвращает массив Item`ов с необходимым именем заявки.
+     * @param key имя необходимой заявки
+     * @return массив Item.
+     */
+    public Item[] findByName(String key) {
+        Item[] itemWithoutNull = new Item[position];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                itemWithoutNull[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemWithoutNull, size);
+    }
+
+    /**
+     * Метод возвращает Item если он найден, если не найден возвращает null.
+     * @param id id заявки
+     * @return массив Item or null.
+     */
+    public Item findById(String id) {
+        for (int index = 0; index < position; index++) {
+            if (items[index].getId().equals(id)) {
+                return items[index];
+            }
+        }
+        return null;
+    }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class TrackerTest {
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenItemNameDontExistThenReturnNull() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("L1"));
+        tracker.add(new Item("L2"));
+        Item result = tracker.findById("SoS");
+        assertNull(result);
+    }
+
+    @Test
+    public void whenAdd4ItemsThenReturn4Items() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("L0"));
+        tracker.add(new Item("L1"));
+        tracker.add(new Item("L2"));
+        tracker.add(new Item("L3"));
+        Item[] all = tracker.findAll();
+        for (int i = 0; i < all.length; i++) {
+            assertEquals(all[i].getName( ), ("L" + i));
+            assertNotNull(all[i].getId());
+        }
+    }
+
+    @Test
+    public void whenItemDontExistThenReturnNull() {
+        Tracker tracker = new Tracker();
+        Item[] all = tracker.findAll();
+        for (int i = 0; i < all.length; i++) {
+            assertNull(all[i]);
+        }
+    }
+
+    @Test
+    public void whenAdd2ItemsWithEqualNameThenReturn2Items() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("L0"));
+        tracker.add(new Item("L1"));
+        tracker.add(new Item("L2"));
+        tracker.add(new Item("L0"));
+        Item[] all = tracker.findByName("L0");
+        assertEquals(all.length, 2);
+    }
+
+    @Test
+    public void whenAddItemsAndNotEqualNameThenReturn2Items() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("L0"));
+        tracker.add(new Item("L1"));
+        tracker.add(new Item("L2"));
+        tracker.add(new Item("L0"));
+        Item[] all = tracker.findByName("L");
+        assertEquals(all.length, 0);
+    }
+}
