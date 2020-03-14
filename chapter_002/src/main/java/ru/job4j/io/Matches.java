73f83f7affe197290c32acdca5146ad74078d81e
Konstantin Kshnyakin <kocmoc007@gmail.com>
2020-03-15 00:11:44 +0300

    2. Scanner и чтение числа из консоли.[#242219]

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -5,40 +5,42 @@ import java.util.Scanner;
 public class Matches {
 
     private final static String message = "Можно взять от 1 до 3 спичек!";
+    private static int inputNumber;
+    private static int numberMatches;
+    private static Scanner input;
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int numberMatches = 11;
+        input = new Scanner(System.in);
+        numberMatches = 11;
         int moveCounter = 0;
         System.out.println("Количество спичек : " + numberMatches);
         System.out.println(message);
         while (numberMatches > 0) {
-            int inputNumber;
             if (moveCounter % 2 == 0) {
-                System.out.println("Ход игрока № 1");
-                inputNumber = Integer.valueOf(input.nextLine( ));
-                if (validateInputNumbers(numberMatches, inputNumber)) {
-                    numberMatches = numberMatches - inputNumber;
-                    moveCounter++;
-                }
+                moveCounter = playerMove(1, moveCounter);
             } else {
-                System.out.println("Ход игрока № 2");
-                inputNumber = Integer.valueOf(input.nextLine( ));
-                if (validateInputNumbers(numberMatches, inputNumber)) {
-                    numberMatches = numberMatches - inputNumber;
-                    moveCounter++;
-                }
+                moveCounter = playerMove(2, moveCounter);
             }
             System.out.println("Количество спичек : " + numberMatches);
             if (numberMatches == 0 && moveCounter % 2 == 0) {
-                System.out.println("Победил игрок № 2");
-            } else if (numberMatches == 0) {
                 System.out.println("Победил игрок № 1");
+            } else if (numberMatches == 0) {
+                System.out.println("Победил игрок № 2");
             }
         }
     }
 
-    public static boolean validateInputNumbers(int numberMatches, int inputNumber) {
+    public static int playerMove(int player, int moveCounter) {
+        System.out.println("Ход игрока № " + player);
+        inputNumber = Integer.valueOf(input.nextLine( ));
+        if (validateInputNumbers(numberMatches)) {
+            numberMatches = numberMatches - inputNumber;
+            return moveCounter++;
+        }
+        return moveCounter;
+    }
+
+    public static boolean validateInputNumbers(int numberMatches) {
         if (inputNumber >= 1 && inputNumber <= 3) {
             if (numberMatches - inputNumber >= 0){
                 return true;
