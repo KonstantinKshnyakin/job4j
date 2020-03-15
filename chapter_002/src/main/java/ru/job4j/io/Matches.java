d564aa98f27898ad976b927693e75978593e9714
Konstantin Kshnyakin <kocmoc007@gmail.com>
2020-03-15 15:02:13 +0300

    2. Scanner и чтение числа из консоли.[#242219]

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -7,40 +7,42 @@ public class Matches {
     private final static String message = "Можно взять от 1 до 3 спичек!";
     private static int inputNumber;
     private static int numberMatches;
+    private static int moveCounter;
     private static Scanner input;
 
     public static void main(String[] args) {
         input = new Scanner(System.in);
         numberMatches = 11;
-        int moveCounter = 0;
+        moveCounter = 0;
         System.out.println("Количество спичек : " + numberMatches);
         System.out.println(message);
         while (numberMatches > 0) {
             if (moveCounter % 2 == 0) {
-                moveCounter = playerMove(1, moveCounter);
+                playerMove(1);
             } else {
-                moveCounter = playerMove(2, moveCounter);
+                playerMove(2);
             }
             System.out.println("Количество спичек : " + numberMatches);
             if (numberMatches == 0 && moveCounter % 2 == 0) {
-                System.out.println("Победил игрок № 1");
-            } else if (numberMatches == 0) {
                 System.out.println("Победил игрок № 2");
+                input.close();
+            } else if (numberMatches == 0) {
+                System.out.println("Победил игрок № 1");
+                input.close();
             }
         }
     }
 
-    public static int playerMove(int player, int moveCounter) {
+    public static void playerMove(int player) {
         System.out.println("Ход игрока № " + player);
-        inputNumber = Integer.valueOf(input.nextLine( ));
-        if (validateInputNumbers(numberMatches)) {
+        inputNumber = Integer.parseInt(input.nextLine( ));
+        if (validateInputNumbers()) {
             numberMatches = numberMatches - inputNumber;
-            return moveCounter++;
+            moveCounter++;
         }
-        return moveCounter;
     }
 
-    public static boolean validateInputNumbers(int numberMatches) {
+    public static boolean validateInputNumbers() {
         if (inputNumber >= 1 && inputNumber <= 3) {
             if (numberMatches - inputNumber >= 0){
                 return true;
