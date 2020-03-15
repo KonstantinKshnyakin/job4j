bba6c4426e2655ce7569c597fafb2d95a318eab2
Konstantin Kshnyakin <kocmoc007@gmail.com>
2020-03-15 21:40:05 +0300

    9.1 Рефакторинг теста @Before @After[#242216]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.stragery.Paint;
 import ru.job4j.stragery.Square;
@@ -13,15 +15,27 @@ import static org.junit.Assert.assertThat;
 
 public class PaintTest {
 
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final String lineSeparator = System.lineSeparator();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
-        String lineSeparator = System.lineSeparator();
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
                         new StringBuilder()
                                 .append("+++++++").append(lineSeparator)
@@ -29,21 +43,15 @@ public class PaintTest {
                                 .append("+     +").append(lineSeparator)
                                 .append("+++++++").append(lineSeparator)
                                 .toString()
-
                 )
         );
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
-        String lineSeparator = System.lineSeparator();
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
                         new StringBuilder()
                                 .append("   +   ").append(lineSeparator)
@@ -51,9 +59,7 @@ public class PaintTest {
                                 .append(" +   + ").append(lineSeparator)
                                 .append("+++++++").append(lineSeparator)
                                 .toString()
-
                 )
         );
-        System.setOut(stdout);
     }
 }
