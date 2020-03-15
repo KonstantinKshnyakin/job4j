bcc9f5e3079de80770e33a8d16cb9914b33303b7
Konstantin Kshnyakin <kocmoc007@gmail.com>
2020-03-15 15:36:41 +0300

    5.1. Тестирование. Подготовка данных.[#242227]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -29,5 +29,30 @@ public class StartUITest {
         int length = tracker.findByName("Fix PC").length;
         assertEquals(2, length);
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                item.getId(),
+                "replaced item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("deleted item");
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        StartUI.delete(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertEquals(replaced, null);
+    }
 }
 
