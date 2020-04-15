920902c2f1507e02805546e6a0baf22de892f595
K.Kshnyakin <kocmoc007@gmail.com>
2020-04-15 23:45:50 +0300

    Travis 5

--- chapter_002/src/test/java/ru/job4j/collection/StringCompareTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -8,7 +8,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StringCompareTest {
-
     @Test
     public void whenStringsAreEqualThenZero() {
         StringCompare compare = new StringCompare();
