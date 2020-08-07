ca225e32d1a989531f70624a323283baef4b2755
K.Kshnyakin <kocmoc007@gmail.com>
2020-08-07 21:30:31 +0300

    Отсортировать департаменты[34352#242175]

--- chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,6 +8,12 @@ public class DepDescComp implements Comparator<String> {
         String[] split1 = o1.split("/", 2);
         String[] split2 = o2.split("/", 2);
         int rst = split2[0].compareTo(split1[0]);
-        return rst == 0 ? o1.compareTo(o2) : rst;
+        if (rst != 0) {
+            return rst;
+        }
+        if (o1.length() != o2.length()) {
+            return o1.length() - o2.length();
+        }
+        return split1[1].compareTo(split2[1]);
     }
 }
